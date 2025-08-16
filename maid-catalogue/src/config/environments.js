b85b046ee74e18eb2dbd2951a9e251eb9798d48b
LIM Yi Kiat <limyikiat97@hotmail.com>
2025-08-16 20:50:21 +0800

    deployment

--- maid-catalogue/src/config/environments.js
+++ maid-catalogue/src/config/environments.js
@@ -7,13 +7,13 @@ const ENVIRONMENTS = {
   },
   
   staging: {
-    API_BASE_URL: 'http://www.yikiat.com:3000',
+    API_BASE_URL: 'http://www.yikiat.com',
     API_PORT: 3000,
     NODE_ENV: 'staging',
   },
   
   production: {
-    API_BASE_URL: 'http://www.yikiat.com:3000',
+    API_BASE_URL: 'http://www.yikiat.com',
     API_PORT: 3000,
     NODE_ENV: 'production',
   },
