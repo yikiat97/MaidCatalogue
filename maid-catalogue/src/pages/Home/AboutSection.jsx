91c5ff3401a5ad044e15c39605e3de61b346129b
LIM Yi Kiat <limyikiat97@hotmail.com>
2025-08-16 19:18:47 +0800

    deployment

--- maid-catalogue/src/pages/Home/AboutSection.jsx
+++ maid-catalogue/src/pages/Home/AboutSection.jsx
@@ -24,14 +24,26 @@ const AboutSection = () => {
             </h2>
             <div className="mb-6 shadow-md rounded-lg p-4 bg-white hover-lift">
               <p className="text-base sm:text-lg md:text-xl font-inter font-light leading-relaxed text-black">
-                " Our mum raised three of us single-handedly. We came from a humble background but hiring a helper was necessary. Unfortunately for us, the experience was often frustrating—high fees, unclear costs, and no real support. It made an already tough time even harder.
+                {/* " Our mum raised three of us single-handedly. We came from a humble background but hiring a helper was necessary. Unfortunately for us, the experience was often frustrating—high fees, unclear costs, and no real support. It made an already tough time even harder.
                 <br /><br />
                 That experience inspired us to start Easy Hire.
                 <br /><br />
                 We believe families deserve honesty, care, and guidance—not added stress. That is why, even as costs rise, we continue to offer a package that waives agency fees for families in need.
                 <br /><br />
-                Easy Hire is not just a business. It is a promise to support others the way we once wished someone had supported us. "
-              </p>
+                Easy Hire is not just a business. It is a promise to support others the way we once wished someone had supported us. " */}
+                <br /><br />
+                A Note From Our Founder 
+                <br /><br />
+                Hello, I’m Li Ling  
+                <br /><br />
+                I started this agency because I want to provide affordable help to all Singaporean families who need it; the kind of help I wished my mum had. 
+                My mum singlehandedly raised me and my two younger sisters, and I grew up watching her struggle with high agency fees, hidden charges, and poor support whenever we faced issues with helpers. 
+                <br /><br />
+                Easy Hire is my way of doing things differently. 
+                I pledge to retain a $0 service fee package for our new Myanmar helper.  
+                I promise my clients honesty, transparent pricing structure, and full support from the start to every step after your helper joins your home. 
+                Because here, you’re not just hiring a worker,  you’re choosing someone who will be caring for your love ones and who will be living under the same roof as you. 
+                </p>
             </div>
             <Button variant="primary" size="medium" className="hover-scale">
               FIND A HELPER
